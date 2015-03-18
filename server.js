056426c17b4055c08070a95c559472f4a224d45f
Stolbukhin Maksim <maxstbn@gmail.com>
2015-03-18 14:40:49 +0300

    add animate

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var express = require('express');
+var mockupSection = require('./mockups/index');
 var app = express();
 
 app.use(express.static('build'));
 app.set('view engine', 'jade');
+
+app.get('/api/v1/Section/Get/:id', function (req, res) {
+	console.log('/api/v1/Section/Get/' + req.params.id);
+	setTimeout(function () {
+		res.send(mockupSection.index[req.params.id])
+	}, 500)
+})
 
 app.get('/*', function (req, res) {
 	res.render('index');
