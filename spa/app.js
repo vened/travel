0c1fd45316209dcca6ba98179d036d79b0d7b7cd
Maxim Stolbukhin <maxstbn@gmail.com>
2015-03-19 14:14:14 +0300

    Базовая разметка ngMaterial

--- spa/app.js
+++ spa/app.js
@@ -27,10 +27,30 @@ app.run(function ($rootScope, $state, $stateParams) {
 });
 
 
-app.controller('AppController', function ($scope, $mdSidenav) {
-    $scope.toggleSidenav = function (menuId) {
-        $mdSidenav(menuId).toggle();
-    };
+app.config(function ($mdThemingProvider) {
+    $mdThemingProvider.definePalette('amazingPaletteName', {
+        '50': 'ffebee',
+        '100': 'ffcdd2',
+        '200': 'ef9a9a',
+        '300': 'e57373',
+        '400': '333333',
+        '500': '212121',
+        '600': 'cccccc',
+        '700': 'cccccc',
+        '800': '333333',
+        '900': 'ff8a80',
+        'A100': 'ff8a80',
+        'A200': 'ff5252',
+        'A400': 'ff1744',
+        'A700': 'd50000',
+        'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
+                                            // on this palette should be dark or light
+        'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
+            '200', '300', '400', 'A100'],
+        'contrastLightColors': undefined    // could also specify this if default was 'dark'
+    });
+    $mdThemingProvider.theme('default')
+        .primaryPalette('amazingPaletteName')
 });
 
 
