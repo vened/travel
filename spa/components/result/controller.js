517743018d25b7baa66a057dfa065bbbf08c1036
maxstbn <maxstbn@gmail.com>
2015-08-07 18:13:51 +0300

    бесконечный скролл

--- spa/components/result/controller.js
+++ spa/components/result/controller.js
@@ -1,4 +1,4 @@
-appComponents.controller('ResultController', function ($scope, $stateParams, $filter, SearchResultServices) {
+appComponents.controller('ResultController', function ($rootScope, $scope, $timeout, $stateParams, $filter, SearchResultServices) {
 
     //https://inna.ru/api/v1/Packages/SearchHotels?DepartureId=6733&ArrivalId=1735&StartVoyageDate=2015-05-11&EndVoyageDate=2015-05-17&TicketClass=0&Adult=2&HotelId=&TicketId=&AddFilter=true&_=1428330728011
     $stateParams.ArrivalId = 1735;
@@ -8,44 +8,22 @@ appComponents.controller('ResultController', function ($scope, $stateParams, $fi
     $scope.hotels = null;
     SearchResultServices.getResultSearch($stateParams)
         .success(function (data) {
-
             $scope.hotels = data.Hotels;
 
-            console.log($scope.hotels);
-            //$scope.datasource = {};
-            //$scope.datasource.get = function (index, count, success) {
-            //    return $timeout(function () {
-            //        result = [];
-            //        for (var i = 0; i < data.length; i++) {
-            //            result.push(data[i]);
-            //        }
-            //        return success(result);
-            //    }, 100);
-            //};
-
-
-            //$scope.datasource = {
-            //    get: function (index, count, success) {
-            //        console.log(index);
-            //        console.log(count);
-            //        console.log(success);
-            //$timeout(function () {
-            //    var start = Math.max(0, index);
-            //    var end = Math.min(index + count, 100);
-            //
-            //    var results = [];
-            //    for (var i = start; i < end; i++) {
-            //        results.push("Index: " + i);
-            //    }
-            //
-            //    success(results);
-            //}, 100);
-            //}
-            //};
-
+            var datasource = {};
+            datasource.get = function (index, count, success) {
+                return $timeout(function () {
+                    var result = [];
+                    var i, j, ref, ref1;
+                    for (i = j = ref = index, ref1 = index + count - 1; ref <= ref1 ? j <= ref1 : j >= ref1; i = ref <= ref1 ? ++j : --j) {
+                        result.push($scope.hotels[i]);
+                        //result.push("item #" + i);
+                    }
+                    return success(result);
+                }, 100);
+            };
+            $scope.datasource = datasource;
 
         });
-    
-    
 
 })
